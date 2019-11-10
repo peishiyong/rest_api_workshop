dbe7956bc76fb7747780930ab206286587129c44
peishiyong <pyong@zendesk.com>
2019-11-10 15:14:36 +1100

    success! 200

--- lib/ticket_client.rb
+++ lib/ticket_client.rb
@@ -7,10 +7,16 @@ class TicketClient
   BASE_URL='https://z3ndtapiador.zendesk.com'
 
   def list_tickets
-    raise NotImplementedError
     # API doc: https://developer.zendesk.com/rest_api/docs/support/tickets#list-tickets
 
-    response = nil # make the faraday call
+    endpoint = "/api/v2/tickets.json"
+    query_param = [
+      'sort_order=desc',
+      'sort_by=created_at',
+      'per_page=15'
+    ].join('&')
+
+    response = connection.get("#{endpoint}?#{query_param}")
     print_response(response)
   end
 
@@ -50,7 +56,7 @@ class TicketClient
     @connection ||= begin
                       Faraday.new(BASE_URL) do |conn|
                         conn.adapter(Faraday.default_adapter)  # make requests with Net::HTTP
-                        # conn.authorization(:Bearer, ENV['API_TOKEN'])
+                        conn.authorization(:Bearer, ENV['API_TOKEN'])
                         # conn.headers['content-type'] = 'application/json'
                       end
                     end
