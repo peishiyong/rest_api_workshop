5d45d97c8ec9cf642ef7829afddf601c203ed9a1
peishiyong <pyong@zendesk.com>
2019-11-11 14:35:42 +1100

    print

--- example.rb
+++ example.rb
@@ -2,4 +2,4 @@ require 'faraday'
 
 # Faraday docs https://lostisland.github.io/faraday/usage/
 response = Faraday.get('http://artii.herokuapp.com/make?text=hello%20world')
-response.body
+puts response.body
